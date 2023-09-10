af8a55ef4b86c6f89908bd44d80076d7afa58423
ejmg <elias.jm.garcia@gmail.com>
2023-09-09 20:46:46 -0500

    setting up eslint to actually work and be used

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
-
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: ["class"],
   content: [
-    './src/pages/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
-    './src/app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -75,6 +74,6 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
 
 export default config;
