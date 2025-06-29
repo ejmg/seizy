ac430e11dfdfd6630f700dda5dd104fa00d648a6
ejmg <elias.jm.garcia@gmail.com>
2025-06-29 18:30:37 -0500

    this is now a react router project

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreateSeizureEvent from "@/components/CreateEvent";
-
-export default function Home () {
-  return (
-    <main className="flex min-h-screen flex-col items-center">
-      <CreateSeizureEvent/>
-    </main>
-  );
-}
--- src/components/NavBar/index.tsx
+++
@@ -1,9 +0,0 @@
-const Navbar = async ({}) => {
-  return (
-    <div className="bg-indigo-900 w-full h-10 text-slate-50">
-      <p>Navbar</p>
-    </div>
-  );
-};
-
-export default Navbar;
