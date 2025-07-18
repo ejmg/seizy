bb537e5e053d2e32d66c2449ae78852a6e6531b4
ejmg <elias.jm.garcia@gmail.com>
2025-07-18 17:40:19 -0500

    dashboard now has responsive stylings

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -3,6 +3,7 @@ import { Calendar } from "~/components/ui/calendar";
 import type { Pet, SeizureWithPet } from "~/lib/types";
 import { Log } from "./logs";
 import { Link } from "react-router";
+import { Button } from "./ui/button";
 
 interface DashboardProps {
   seizures: SeizureWithPet[];
@@ -11,17 +12,17 @@ interface DashboardProps {
 
 const PetCard = ({ pet }: { pet: Pet }) => {
   return (
-    <div className="bg-white border rounded-xl p-6 shadow-sm hover:shadow-md transition-shadow">
-      <div className="flex items-center space-x-4 mb-4">
-        <Avatar className="h-20 w-20">
+    <div className="bg-card border rounded-xl p-3 sm:p-6 shadow-sm hover:shadow-md transition-shadow">
+      <div className="flex items-center space-x-2 sm:space-x-4 mb-2 sm:mb-4">
+        <Avatar className="h-12 sm:h-20 w-12 sm:w-20">
           <AvatarImage src={pet.avatar_url} />
-          <AvatarFallback className="text-lg">
+          <AvatarFallback className="text-sm sm:text-lg">
             {pet.name.charAt(0).toUpperCase()}
           </AvatarFallback>
         </Avatar>
         <div>
-          <h3 className="text-xl font-semibold">{pet.name}</h3>
-          <p className="text-gray-600">
+          <h3 className="text-lg sm:text-xl font-semibold">{pet.name}</h3>
+          <p className="text-sm sm:text-base text-gray-600">
             {pet.species} {pet.breed && `– ${pet.breed}`}
           </p>
         </div>
@@ -36,18 +37,12 @@ const PetCard = ({ pet }: { pet: Pet }) => {
       )}
 
       <div className="flex space-x-2">
-        <Link
-          to={`/edit-pet/${pet.id}`}
-          className="flex-1 text-center bg-indigo-600 text-primary-foreground py-2 rounded-lg hover:bg-indigo-800"
-        >
-          Edit
-        </Link>
-        <Link
-          to={`/logs?pet=${pet.id}`}
-          className="flex-1 text-center bg-gray-100 py-2 rounded-lg hover:bg-gray-200"
-        >
-          View Logs
-        </Link>
+        <Button asChild>
+          <Link to={`/edit-pet/${pet.id}`}>Edit</Link>
+        </Button>
+        <Button asChild variant={"secondary"}>
+          <Link to={`/logs?pet=${pet.id}`}>Logs</Link>
+        </Button>
       </div>
     </div>
   );
@@ -55,30 +50,30 @@ const PetCard = ({ pet }: { pet: Pet }) => {
 
 export function Dashboard({ seizures, pets }: DashboardProps) {
   return (
-    <div className="flex w-full p-8 space-y-4">
-      <div className="flex flex-col w-2/3 gap-4">
+    <div className="flex flex-col lg:flex-row w-full p-3 sm:p-8 space-y-4 lg:space-y-0 lg:space-x-4">
+      <div className="flex flex-col lg:w-2/3 gap-4">
         <div className="flex flex-col gap-4">
-          <h2 className="text-2xl font-semibold">Overview</h2>
-          <div className="flex flex-col gap-2 w-5/6">
+          <h2 className="text-xl sm:text-2xl font-semibold">Overview</h2>
+          <div className="flex flex-col gap-2 w-full lg:w-5/6">
             {pets.map((pet) => (
               <PetCard pet={pet} />
             ))}
           </div>
         </div>
         <div className="flex flex-col gap-4">
-          <h2 className="text-2xl font-semibold">Recent Logs</h2>
-          <div className="flex flex-col items-start gap-2 w-5/6">
+          <h2 className="text-xl sm:text-2xl font-semibold">Recent Logs</h2>
+          <div className="flex flex-col items-start gap-2 w-full lg:w-5/6">
             {seizures.map((seizure) => (
               <Log key={seizure.id} seizure={seizure} />
             ))}
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-4 w-1/3">
-        <h2 className="text-2xl font-semibold">Calendar</h2>
+      <div className="flex flex-col gap-4 lg:w-1/3">
+        <h2 className="text-xl sm:text-2xl font-semibold">Calendar</h2>
         <Calendar className="w-full rounded-xl bg-card" />
         <div className="flex flex-col gap-4">
-          <h2 className="text-2xl font-semibold">Upcoming</h2>
+          <h2 className="text-xl sm:text-2xl font-semibold">Upcoming</h2>
           <div className="flex gap-4 bg-pink-50 border rounded-xl p-2">
             <div className="flex items-center gap-2">
               <Avatar className="h-14 w-14">
